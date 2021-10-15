bdb3d165a4259740bcd6eebcaab02658bfcc6213
kstechgiig <92563789+kstechgiig@users.noreply.github.com>
2021-10-15 11:28:58 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,10 +3,5 @@ class Test
 public ststic void main(String[] args)
 {
 	System.out.println("Hello");
-	System.out.println("Extended Hello");
-	System.out.println("Next Hello");
-	System.out.println("Remote merge branch");
-	System.out.println("Another merge branch);
-	System.out.println("This is to test jenkins");
 }
 }
